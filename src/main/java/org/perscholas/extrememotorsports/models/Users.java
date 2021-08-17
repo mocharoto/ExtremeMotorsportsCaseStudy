ce3729c1cd58fdf8c6b17278b3cc9891737bcc90
mocharoto <julian.yi26@gmail.com>
2021-08-17 14:29:27 -0700

    Updating Users.java

--- src/main/java/org/perscholas/extrememotorsports/models/Users.java
+++ src/main/java/org/perscholas/extrememotorsports/models/Users.java
@@ -1,16 +1,33 @@
 package org.perscholas.extrememotorsports.models;
 
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull(message = "ID can't be null")
     long uId;
+    @NotBlank(message = "Username cannot be null")
     String username;
+    @Email(message = "Try adding a correct email")
     String email;
 
 }
